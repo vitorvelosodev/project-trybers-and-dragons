056554625434dc8ea0bf81b45733f16a0a561d6e
Vitor Veloso <vitorvelosodev@gmail.com>
2023-07-12 10:00:00 -0300

    :sparkles: Req 04 ok

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,33 @@
+import { EnergyType } from '../Energy';
+
+abstract class Archetype {
+  protected _name : string;
+  protected _special : number;
+  protected _cost : number;
+
+  constructor(name : string) {
+    this._name = name;
+    this._special = 0;
+    this._cost = 0;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get special() {
+    return this._special;
+  }
+
+  get cost() {
+    return this._cost;
+  }
+
+  static createdArchetypeInstances() {
+    throw new Error('Not implemented');
+  }
+
+  abstract get energyType() : EnergyType;
+}
+
+export default Archetype;
